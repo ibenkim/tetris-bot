6c78a4671d0837212b069406c7cca1c4c8c5de49
ibenkim <125783238+ibenkim@users.noreply.github.com>
2025-11-21 14:42:37 -0800

    update board

--- board.cpp
+++ board.cpp
@@ -12,25 +12,25 @@ Board::Board() {
 }
 
 Board::Board(int rows, int cols) {
-   nRows = rows;
-   nCols = cols;
-   grid = new char*[nRows];
-   for(int i = 0; i < nRows; i++) {
-      grid[i] = new char[nCols];
-   }
+    nRows = rows;
+    nCols = cols;
+    grid = new char*[nRows];
+    for(int i = 0; i < nRows; i++) {
+        grid[i] = new char[nCols];
+    }
 }
 
 Board::~Board() {
-   for(int i = 0; i < nRows; i++) {
+    for(int i = 0; i < nRows; i++) {
       delete [] grid[i];
-   }
+    }
    delete [] grid;
 }
 
 void Board::BoardReset() {
     for(int i = 0; i < nRows; i++) {
       delete [] grid[i];
-   }
+    }
    delete [] grid;
 
    nRows = 20;
@@ -40,3 +40,16 @@ void Board::BoardReset() {
       grid[i] = new char[nCols];
    }
 }
+
+void Board::LineClear(int row) {
+    for(int i = nRows-row-1; i >= 0; i++) {
+        delete [] grid[i];
+    }
+
+    for(int i = nRows-row-1; i < nRows-1; i++) {
+        for(int j = 0; j < nCols; j++) {
+            grid[i-row][j] = grid[i][j];
+        }
+    } 
+}
+
