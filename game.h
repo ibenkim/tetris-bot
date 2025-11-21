c6c8ebab08fea812be785f0071f7296c6879b9f4
Kyle Yuen <45528039+kyleyuen@users.noreply.github.com>
2025-11-21 14:23:17 -0800

    Added game.h file

--- game.h
+++ game.h
@@ -1,31 +1,42 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "board.h"
+#include "piece.h"
 
+class Game{
+public:
+    Game();
+    ~Game();
 
+    void Start();
+    void Update();
+    void HandleInput(char key);
 
+    int getScore() const;
+    int getLevel() const;
+    bool isGameOver() const;
 
+private:
+    Board board;
+    Piece* currentPiece;
+    Piece* nextPiece; 
 
+    int score;
+    int level;
+    bool gameOver;
 
+    //Piece Management 
+    void SpawnPiece();
+    bool MoveCurrentPiece(int dx, int dy);
+    void RotateCurrentPiece(bool clockwise);
 
+     // Locking + collision
+    void LockPiece();
+    bool Collision(Piece* p, int newRow, int newCol);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // Line clearing
+    void ClearFullLines();
+};
 
 #endif
