351845d5f68650ed7987ac89ab172b7d0aa32fa2
Alex Luo <160722845+Mathdonutsociety@users.noreply.github.com>
2025-12-05 15:10:35 -0800

    Update Tpiece.h

--- Tpiece.h
+++ Tpiece.h
@@ -17,16 +17,6 @@
 struct Tpiece : public Piece {
     //ctor
     Tpiece(int board_rows, int board_cols, char** board_state); 
-
-    // Rotations
-    void Rotate_L(); 
-    void Rotate_R(); 
-
-    // Translations
-    void step_L(); 
-    void step_R(); 
-    void step_D();
-    void hard_drop(); 
 };
 
 
