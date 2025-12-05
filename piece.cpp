82a5acd00789f6e7db69870c43091c4e4d26c40b
Alex Luo <160722845+Mathdonutsociety@users.noreply.github.com>
2025-12-05 15:09:15 -0800

    Update piece.cpp

--- piece.cpp
+++ piece.cpp
@@ -2,48 +2,99 @@
 #include "piece.h"
 
 // ctor
-Piece::Piece(char c, int board_rows, int board_cols, char **board_state) {
-  max_up = board_rows;
-  max_right = board_cols;
+Piece::Piece(int board_rows, int board_cols, char** board_state) {
+    max_up = board_rows; 
+    max_right = board_cols;
 
-  row = max_up;
-  col = max_right / 2;
+    row = max_up; 
+    col = max_right / 2; 
 
-  type = c;
-  rot = 0;
+    rot = 0; 
 
-  board = board_state;
+    board = board_state; 
 }
+
+bool Piece::isValidPos(int r, int c, int rotation) {
+    for (int i = 0; i < 4; i++) {
+        int colPos = Xcoords[rotation][i] + c; 
+        int rowPos = Ycoords[rotation][i] + r; 
+
+        if (colPos < 0 || colPos >= max_right) return false; 
+        if (rowPos < 0) return false; 
+
+        if (board[rowPos][colPos] != 0) return false; 
+    }
+
+    return true; 
+}
+
 
 // rotate CCW
 // implement in each piece class
-void Piece::Rotate_L() { rot = (rot - 1) % 4; }
+bool Piece::Rotate_L() {
+    if (isValidPos(row, col, (rot - 1) % 4)) {
+        rot = (rot - 1) % 4; 
+        return true; 
+    }
+
+    return false; 
+}
 // rotate CW
-void Piece::Rotate_R() { rot = (rot + 1) % 4; }
+bool Piece::Rotate_R() {
+    if (isValidPos(row, col, (rot + 1) % 4)) {
+        rot = (rot + 1) % 4; 
+        return true; 
+    }
+
+    return false; 
+}
 
 // Translations
+// implement in each piece class
+bool Piece::step_L() {
+    if (isValidPos(row, col - 1, rot)) {
+        col--; 
+        return true; 
+    }
+
+    return false; 
+}
+bool Piece::step_R() {
+    if (isValidPos(row, col + 1, rot)) {
+        col++; 
+        return true; 
+    }
+
+    return false; 
+}
+bool Piece::step_D() {
+    if (isValidPos(row - 1, col, rot)) {
+        row--; 
+        return true; 
+    }
+    
+    return false; 
+}
 
 // implement in each piece class
-void Piece::step_L() {
-  if (col > leftN[rot]) {
-    col -= 1;
-  }
-}
-void Piece::step_R() {
-  if (col < max_right - rightN[rot] - 1) {
-    col += 1;
-  }
-}
-void Piece::step_D() {
-  if (row > 0) {
-    row -= 1;
-  }
+void Piece::hard_drop() {
+    while (isValidPos(row - 1, col, rot)) {
+        row--; 
+    }
 }
 
-// implement in each piece class
-void Piece::hard_drop() {}
+int Piece::getRow() {
+    return row; 
+}
+int Piece::getCol() {
+    return col; 
+}
+int Piece::getType() {
+    return type; 
+}
+int Piece::getRotation() {
+    return rot; 
+}
 
-int Piece::getRow() { return row; }
-int Piece::getCol() { return col; }
-int Piece::getType() { return type; }
-int Piece::getRotation() { return rot; }
+
+
